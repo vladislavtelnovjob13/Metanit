94d33f602481eb25152d4d51f4516e71bc6ebe9a
Владислав <vladislav.telnov.joboffer@gmail.com>
2021-11-02 16:29:12 +0300

    Добавьте файлы проекта.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string? name = Console.ReadLine();       // вводим имя
+            Console.WriteLine($"Привет {name}");
+        }
+    }
+}
