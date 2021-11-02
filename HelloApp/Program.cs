ea8e05138139cc5c94c08460d757e44df0ad9740
Владислав <vladislav.telnov.joboffer@gmail.com>
2021-11-02 20:15:08 +0300

    Дошел до пункта 2.5, Консольный ввод

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -9,6 +9,16 @@ namespace HelloApp
             Console.Write("Введите свое имя: ");
             string? name = Console.ReadLine();       // вводим имя
             Console.WriteLine($"Привет {name}");
+            Console.ReadLine();
+            TRUETH(name);
+        }
+
+        static void TRUETH( string a)
+        {
+            string name = "Tom";
+            int age = 34;
+            double height = 1.7;
+            Console.WriteLine($"Имя: {name}  \nВозраст: {age}  \nРост: {height}м");
         }
     }
 }
